1cf574bb0eb50a89e62d64c09d4bb57a4259f3f9
Zzathy <izzafathony27@gmail.com>
2025-04-10 18:03:53 +0700

    fix(route): mistake on route index

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,13 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index')->name('index');
-});
+    return view('index');
+})->name('index');
 
 Route::resource('categories', CategoryController::class);
 Route::resource('products', ProductController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->name('register');
 Route::post('register', [RegisterController::class, 'store']);
+
+Route::post('logout', [LoginController::class, 'destroy'])->name('logout');
