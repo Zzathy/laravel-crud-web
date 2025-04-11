8a63b7e6fb1b1de38c15553b90f4e0cd9590d025
Zzathy <izzafathony27@gmail.com>
2025-04-11 18:29:21 +0700

    refactor(product): make the migration/table easier to understand

--- database/migrations/2025_03_21_164307_create_products_table.php
+++ database/migrations/2025_03_21_164307_create_products_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->ulid('id')->primary();
             $table->string('name');
-            $table->text('description')->nullable();
-            $table->integer('price');
             $table->string('category_id');
             $table->foreign('category_id')->references('id')->on('categories');
+            $table->text('description')->nullable();
+            $table->integer('price');
             $table->timestamps();
         });
     }
