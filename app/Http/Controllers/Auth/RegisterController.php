b304cb11c1f5c23f7bc87eadc427f8a44c476814
Zzathy <izzafathony27@gmail.com>
2025-04-15 15:04:48 +0700

    fix(register): fix the request path

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreRegisterRequest;
-use App\Http\Requests\UpdateRegisterRequest;
+use App\Http\Requests\Auth\StoreRegisterRequest;
+use App\Http\Requests\Auth\UpdateRegisterRequest;
 use App\Models\User;
 
 class RegisterController extends Controller
